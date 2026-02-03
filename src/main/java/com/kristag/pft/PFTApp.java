6b5dd758ddc6ba652e0307831c07a52c35c8d887
Krista G <gemini.kris@proton.me>
2026-02-03 11:27:24 +0100

    Fix: bump AssertJ to 3.27.7 (CVE-2026-24400)

---
+++ src/main/java/com/kristag/pft/PFTApp.java
@@ -0,0 +1,11 @@
+package com.kristag.pft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PFTApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PFTApp.class, args);
+    }
+}
